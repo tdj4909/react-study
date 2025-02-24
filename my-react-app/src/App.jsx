50e5936551bbb9ed332fe182664b622d8e9d06a8
tdj4909 <tdj4909@gmail.com>
2025-02-24 13:25:48 +0900

    update

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,4 +1,6 @@
 import Hello from "./Hello";
+import Counter from "./Counter";
+import UserName from "./UserName";
 
 function App(){
   return (
@@ -7,6 +9,8 @@ function App(){
       <Hello name="철수" />
       <Hello name="영희" />
       <Hello name="민수" />
+      <Counter />
+      <UserName />
     </div>
   );
 }
---
+++ my-react-app/src/Counter.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <p>현재 카운트: {count}</p>
+            <button onClick={() => setCount(count + 1)}>+1 증가</button>
+        </div>
+    );
+}
+
+export default Counter;
