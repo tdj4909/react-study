4aa8ef096506f59407389f07a974726f918f8a2b
tdj4909 <tdj4909@gmail.com>
2025-02-24 13:29:49 +0900

    update

--- my-react-app/src/Counter.jsx
+++ my-react-app/src/Counter.jsx
@@ -3,10 +3,14 @@ import { useState } from "react";
 function Counter() {
     const [count, setCount] = useState(0);
 
+    const handleClick = () => {
+        setCount(count + 1);
+    };
+
     return (
         <div>
             <p>현재 카운트: {count}</p>
-            <button onClick={() => setCount(count + 1)}>+1 증가</button>
+            <button onClick={handleClick}>+1 증가</button>
         </div>
     );
 }
