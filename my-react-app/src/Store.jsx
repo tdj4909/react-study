796948aef8e6f09d815b512404c21d889eb6014b
tdj4909 <tdj4909@gmail.com>
2025-02-25 15:54:56 +0900

    update

--- my-react-app/src/Store.jsx
+++ my-react-app/src/Store.jsx
@@ -1,10 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit";
 import counterReducer from "./CounterSlice";
+import loggerMiddleware from "./LoggerMiddleware";
 
 const store = configureStore({
     reducer: {
         counter: counterReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(loggerMiddleware),
+    devTools: process.env.NODE_ENV !== "production",
 });
 
 export default store;
