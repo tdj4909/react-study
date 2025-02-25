1f7677de95910363305aaf1be022890ac43e56a3
tdj4909 <tdj4909@gmail.com>
2025-02-25 17:48:46 +0900

    update

---
+++ my-react-app/src/Parent.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Child = React.memo(({name}) => {
+    console.log('Child Rendered');
+    return <p>{name}</p>;
+});
+
+function Parent() {
+    const [counter, setCounter] = React.useState(0);
+
+    return (
+        <div>
+            <button onClick={() => setCounter(counter + 1)}>Increment</button>
+            <p>Counter: {counter}</p>
+            <Child name="John" />
+        </div>
+    );
+}
+
+export default Parent;
