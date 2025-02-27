ba79bb366c0cb1506c183d0162723eb6a5d19659
tdj4909 <tdj4909@gmail.com>
2025-02-27 10:46:08 +0900

    update

--- my-react-app/src/Store.jsx
+++ my-react-app/src/Store.jsx
@@ -1,23 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
 import counterReducer from "./CounterSlice";
-import storage from "redux-persist/lib/storage";
-import { persistReducer, persistStore } from "redux-persist";
-import {thunk} from "redux-thunk";
-
-const persistConfig = {
-    key: "root",
-    storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, counterReducer);
 
 const store = configureStore({
     reducer: {
-        counter: persistedReducer,
+        counter: counterReducer,
     },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({serializableCheck: false}).concat(thunk),
 });
 
-export const persistor = persistStore(store);
 export default store;
