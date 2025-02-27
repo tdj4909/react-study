6287ddb7403d949e5f4a8cfb0e3bcc33cb54f05a
tdj4909 <tdj4909@gmail.com>
2025-02-27 09:38:05 +0900

    update

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,21 +1,22 @@
+import { UserProvider } from "./UserContext";
 import Profile from "./Profile";
 import Counter from "./Counter";
 import ButtonClick from "./ButtonClick";
 import PostList from "./PostList";
 
-function App(){
+function App() {
   return (
-    <div>
-      <h1>리액트 데이터 관리 실습</h1>
-      
-      <Profile name="홍길동" age={25} />
+    <UserProvider>
+      <div>
+        <h1>리액트 데이터 관리 실습</h1>
 
-      <Counter />
-
-      <ButtonClick />
-
-      <PostList />
-    </div>
+        <Profile/>
+        <Counter />
+        <ButtonClick />
+        <PostList />
+        
+      </div>
+    </UserProvider>
   );
 }
 
--- my-react-app/src/Profile.jsx
+++ my-react-app/src/Profile.jsx
@@ -1,9 +1,18 @@
+import { useContext } from "react";
+import UserContext from "./UserContext";
 
-function Profile({name, age}) {
+function Profile() {
+    const {user, setUser} = useContext(UserContext);
+
     return (
         <div>
-            <h2>이름: {name}</h2>
-            <p>나이: {age}세</p>
+            <h2>프로필</h2>
+            <p>이름: {user.name}</p>
+            <p>나이: {user.age}</p>
+
+            <button onClick={() => setUser({...user, age: user.age + 1})}>
+                나이 증가
+            </button>
         </div>
     );
 }
