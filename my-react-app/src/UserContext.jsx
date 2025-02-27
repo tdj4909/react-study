00fc6e4ba360b646d97f38f5a180808bfebc837d
tdj4909 <tdj4909@gmail.com>
2025-02-27 09:22:14 +0900

    update

--- my-react-app/src/UserContext.jsx
+++ my-react-app/src/UserContext.jsx
@@ -1,3 +1,15 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-export const UserContext = createContext(null);
+const UserContext = createContext();
+
+export function UserProvider({children}) {
+    const [user, setUser] = useState({ name: "홍길동", age: 25});
+
+    return (
+        <UserContext.Provider value={{user,setUser}}>
+            {children}
+        </UserContext.Provider>
+    );
+}
+
+export default UserContext;
